c3d809e1008f033cf0c0730038a3008d13b132b7
Danil <privet.ya.danil@gmail.com>
2015-04-28 20:24:58 +0300

    Added person list screen

--- app/src/main/java/com/fiivt/ps31/callfriend/PersonActivity.java
+++ app/src/main/java/com/fiivt/ps31/callfriend/PersonActivity.java
@@ -1,31 +1,29 @@
 package com.fiivt.ps31.callfriend;
 
+import android.app.Activity;
 import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-
-import java.util.List;
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import com.fiivt.ps31.callfriend.AppDatabase.AppDb;
 import com.fiivt.ps31.callfriend.AppDatabase.Event;
 import com.fiivt.ps31.callfriend.AppDatabase.Person;
+import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
+/**
+ * Created by Данил on 24.04.2015.
+ */
+public class PersonActivity extends Activity {
 
-public class EventsActivity extends ActionBarActivity {
 
     public AppDb database;
 
@@ -43,19 +41,19 @@ public class EventsActivity extends ActionBarActivity {
             Person p = db.getPerson(1);
             Person p2 = db.getPerson(2);
             Person p3 = db.getPerson(3);
-            Event e = new Event("РџРѕР·РґР°РІРёС‚СЊ СЃ РґРЅРµРј РІР°С„Р»Рё", new Date(), p);
+            Event e = new Event("Поздавить с днем вафли", new Date(), p);
             db.addEvent(e);
 
             List<Event> oldEvents = db.getExpiredEvents();
             List<Event> allEvents = db.getEvents();
 
-            Event e2 = new Event("РџРѕР·РІР°С‚СЊ РІ РєРёРЅРѕ", new Date(), p2);
+            Event e2 = new Event("Позвать в кино", new Date(), p2);
             db.addEvent(e2);
 
-            Event e3 = new Event("РџРѕР·РґР°РІРёС‚СЊ СЃ РґРЅРµРј РІР°С„Р»Рё", new Date(), p3);
+            Event e3 = new Event("Поздавить с днем вафли", new Date(), p3);
             db.addEvent(e3);
 
-            Event e4 = new Event("РџРѕР·РІР°С‚СЊ РІ РјР°Рє", new Date(), p3);
+            Event e4 = new Event("Позвать в мак", new Date(), p3);
             db.addEvent(e4);
 
 //            db.deleteEvent(allEvents.get(0).getId());
@@ -67,22 +65,18 @@ public class EventsActivity extends ActionBarActivity {
         }
     }
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         database = new AppDb(this);
         test(database);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.person_list_layout);
 
-        ListView eventsListView = (ListView) findViewById(R.id.events_list_view);
-        final List<Event> event = database.getEvents();
-        ArrayAdapter adapter = new MySimpleArrayAdapter(this, event);
-        eventsListView.setAdapter(adapter);
-
+        ListView personsListView = (ListView) findViewById(R.id.person_list_view);
+        final List<Person> person = database.getPersons();
+        ArrayAdapter personAdapter = new PersonArrayAdapter(this, person);
+        personsListView.setAdapter(personAdapter);
     }
 
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -95,27 +89,23 @@ public class EventsActivity extends ActionBarActivity {
         return true;
     }
 
-
     @Data
-    static class EventViewHolder {
+    static class PersonViewHolder {
         private TextView name;
-        private TextView title;
         private ImageView image;
-        private TextView daysLeft;
 
-        public void setEventValues(Event event) {
-            name.setText(event.getPerson().getName());
-            title.setText(event.getTitle());
+        public void setPersonValues(Person person) {
+            name.setText(person.getName());
             //image.setImageResource(R.mipmap.ic_user);
         }
     }
 
-    public class MySimpleArrayAdapter extends ArrayAdapter<Event> {
+    public class PersonArrayAdapter extends ArrayAdapter<Person> {
         private final Context context;
-        private final List<Event> values;
+        private final List<Person> values;
 
-        public MySimpleArrayAdapter(Context context, List<Event> values) {
-            super(context, R.layout.row_list_contact, values);
+        public PersonArrayAdapter(Context context, List<Person> values) {
+            super(context, R.layout.row_list_cont, values);
             this.context = context;
             this.values = values;
         }
@@ -123,9 +113,9 @@ public class EventsActivity extends ActionBarActivity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             View view = getViewWithHolder(convertView, parent);
-            EventViewHolder viewHolder = (EventViewHolder) view.getTag();
-            Event event = values.get(position);
-            viewHolder.setEventValues(values.get(position));
+            PersonViewHolder viewHolder = (PersonViewHolder) view.getTag();
+            Person person = values.get(position);
+            viewHolder.setPersonValues(person);
             return view;
         }
 
@@ -141,14 +131,12 @@ public class EventsActivity extends ActionBarActivity {
 
         private View createNewView(ViewGroup parent) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            return inflater.inflate(R.layout.row_list_contact, parent, false);
+            return inflater.inflate(R.layout.row_list_cont, parent, false);
         }
 
-        private EventViewHolder initializeHolder(View view) {
-            EventViewHolder holder = new EventViewHolder();
-            holder.setName((TextView) view.findViewById(R.id.event_list_contact_name));
-            holder.setTitle((TextView) view.findViewById(R.id.event_list_event_title));
-            holder.setDaysLeft((TextView) view.findViewById(R.id.events_days_left));
+        private PersonViewHolder initializeHolder(View view) {
+            PersonViewHolder holder = new PersonViewHolder();
+            holder.setName((TextView) view.findViewById(R.id.person_list_contact_name));
             //holder.setImage((ImageView) view.findViewById(R.id.contactAvatar));
             view.setTag(holder);
             return holder;
@@ -158,22 +146,5 @@ public class EventsActivity extends ActionBarActivity {
         public int getCount() {
             return values.size();
         }
-
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_show_contacts) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
